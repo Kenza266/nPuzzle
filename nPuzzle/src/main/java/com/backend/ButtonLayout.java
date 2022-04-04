b421531d339a0c35178efe04034afc20a32a66c2
Kenza266 <66864019+Kenza266@users.noreply.github.com>
2022-04-05 01:20:20 +0200

    Add files via upload

---
+++ nPuzzle/src/main/java/com/backend/ButtonLayout.java
@@ -0,0 +1,27 @@
+package com.backend;
+
+public class ButtonLayout {
+    double x;
+    double y;
+
+    public ButtonLayout(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+}
